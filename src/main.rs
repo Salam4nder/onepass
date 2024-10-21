d1b27936c255f84e2c7022886e9138c6e0f8c3b3
Salam4nder <kamran.personal@icloud.com>
2024-10-22 00:48:23 +0200

    refactor: move command out

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
-use std::io::{self, Write, Result};
-use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
+mod command;
 
-enum Command {
-    New,
-    Get,
-    Suggest
-}
+use std::io::{self, Write};
+use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
+use crate::command::Command;
 
 fn write(s: &mut StandardStream, msg: &str) -> io::Result<()> {
         writeln!(s, "{}", msg)
@@ -14,29 +11,11 @@ fn write(s: &mut StandardStream, msg: &str) -> io::Result<()> {
 const PARSE_MSG:   &str = "expecting 2 arguments: {{command}} {{target}}";
 const COMMAND_MSG: &str = "expecting {{command}} as first argument: new, get, suggest";
 
-impl Command {
-    fn from_string(s: &str) -> Option<Command> {
-        match s {
-            "new"     => return Some(Command::New),
-            "get"     => return Some(Command::Get),
-            "suggest" => return Some(Command::Suggest),
-            _         => return None,
-        }
-    }
-    fn execute(&self, t: &str) {
-        match self {
-            Command::New     => println!("new command, {t}"),
-            Command::Get     => println!("get command, {t}"),
-            Command::Suggest => println!("suggest command"),
-        }
-    }
-}
-
 fn main() {
     let mut stdout = StandardStream::stdout(ColorChoice::Always);
     stdout.set_color(ColorSpec::new().set_fg(Some(Color::Green))).unwrap_or_default();
 
-    let mut input= String::new();
+    let mut input = String::new();
     let (mut cmd_str, mut target_str) = (String::new(), String::new());
     let std_in = io::stdin();
 
