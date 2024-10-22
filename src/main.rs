5fdceeff06c166bf68560b408b4fdb100c20f0d0
Salam4nder <kamran.personal@icloud.com>
2024-10-22 02:12:58 +0200

    refactor: flag parsing

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,43 @@
+mod file;
 mod command;
 
+use std::env;
 use std::io::{self, Write};
 use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
-use crate::command::Command;
+use crate::command::Kind;
 
 fn write(s: &mut StandardStream, msg: &str) -> io::Result<()> {
         writeln!(s, "{}", msg)
 }
 
-const PARSE_MSG:   &str = "expecting 2 arguments: {{command}} {{target}}";
-const COMMAND_MSG: &str = "expecting {{command}} as first argument: new, get, suggest";
+const PARSE_MSG:   &str = "expecting argument: {{command}}";
+const COMMAND_MSG: &str = "expecting {{command}} as first argument: init, new, get, suggest. example: onepass init";
 
 fn main() {
     let mut stdout = StandardStream::stdout(ColorChoice::Always);
     stdout.set_color(ColorSpec::new().set_fg(Some(Color::Green))).unwrap_or_default();
 
-    let mut input = String::new();
-    let (mut cmd_str, mut target_str) = (String::new(), String::new());
-    let std_in = io::stdin();
+    let _std_in = io::stdin();
 
-    match std_in.read_line(&mut input) {
-        Ok(_) => (),
-        Err(err) => panic!("{err}")
-    }
-        
-    let mut split = input.split_whitespace();
-    if split.clone().count() != 2 {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 1 {
         let _ = write(&mut stdout, PARSE_MSG);
+    }
+    let command_string = &args[0];
+
+    let command_kind: Kind;
+    if let Some(c) = command::Kind::from_string(command_string.as_str()) {
+        command_kind = c;
+    } else {
+        let _ = write(&mut stdout, COMMAND_MSG);
         std::process::exit(2);
     }
-    if let Some(cmd) = split.next() {
-        cmd_str = cmd.to_string();
-    }
-    if let Some(trgt) = split.next() {
-        target_str = trgt.to_string();
+
+    match command_kind {
+        Kind::Init => (),
+        Kind::New => (),
+        Kind::Get => (),
+        Kind::Suggest => (),
     }
 
-    let command :Command;
-    match Command::from_string(cmd_str.as_str()) {
-        Some(c) => command = c,
-        None => {
-            let _ = write(&mut stdout, COMMAND_MSG);
-            std::process::exit(2);
-        }
-
-    }
-    command.execute(target_str.as_str());
 }
