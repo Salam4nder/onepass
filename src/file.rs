37bf2251d3a63b7cf6c5b037d940257f6c60710c
Salam4nder <kamran.personal@icloud.com>
2024-10-27 20:04:01 +0100

    refactor: file.rs

--- src/file.rs
+++ src/file.rs
@@ -3,8 +3,9 @@ use std::io;
 use std::path::PathBuf;
 use std::env;
 
-const DEFAULT_DIR_NAME:  &str = ".onepass";
-const DEFAULT_FILE_NAME: &str = "main.txt";
+pub const DELIMITER:         &str = "|-----|";
+const DEFAULT_DIR_NAME:      &str = ".onepass";
+const DEFAULT_FILE_NAME:     &str = "main.txt";
 
 pub fn file_path() -> PathBuf {
     let home_dir = match env::var("HOME") {
@@ -17,13 +18,11 @@ pub fn file_path() -> PathBuf {
     path
 }
 
-fn create(password: &str) -> io::Result<std::fs::File> {
+pub fn create() -> io::Result<std::fs::File> {
     let path = file_path();
 
     let file = OpenOptions::new()
         .create(true)
-        .append(true)
-        .write(true)
         .open(&path)?;
 
     Ok(file)
@@ -33,7 +32,6 @@ pub fn exists() -> bool {
     let path = file_path();
 
     if let Err(err) = OpenOptions::new().read(true).open(&path) {
-        println!("err from root_exists: {}", err.to_string());
         if err.kind() == io::ErrorKind::NotFound {
             return false;
         } else {
