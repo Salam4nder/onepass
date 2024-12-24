2a5b645c4af32c07334f87d0e161465a9fd9d091
Salam4nder <kamran.personal@icloud.com>
2024-12-24 16:25:20 +0100

    feat: text.rs

---
+++ src/text.rs
@@ -0,0 +1,4 @@
+pub const COMMAND_MSG: &str = "expecting {{command}} as first argument: init, new, get, suggest. example: onepass init";
+pub const MSG_SETUP:     &str = "you are all setup! run `onepass help`";
+pub const MSG_NOT_SETUP: &str = "you are not setup! run `onepass init`";
+pub const MSG_GET:       &str = "expecting resource: e.g - onepass get soundcloud";
