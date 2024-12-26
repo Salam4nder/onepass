182f6eec4eba35eae6df8dd04da401007e6bda11
Salam4nder <kamran.personal@icloud.com>
2024-12-26 22:02:14 +0100

    chore: cleanup

--- src/command.rs
+++ src/command.rs
@@ -2,15 +2,8 @@ use crate::text;
 use crate::file;
 use crate::input;
 use crate::password;
-use std::io::Write;
 use std::io::Stdin;
 use std::sync::atomic::{AtomicBool, Ordering};
-
-use rand::rngs::OsRng;
-use chacha20poly1305::{
-    aead::AeadCore,
-    ChaCha20Poly1305
-};
 
 pub static DONE: AtomicBool = AtomicBool::new(false);
 
@@ -147,64 +140,13 @@ pub fn del(args: Vec<String>) -> Result<(), String> {
     if args.len() < 3 {
         return Err(text::MSG_GET.to_string());
     }
-    let resource_str = &args[2];
-    if input::is_reserved(resource_str) {
+    let res = &args[2];
+    if input::is_reserved(res) {
         return Err("use of reserved keyword".to_string());
     }
-    let master_password = input::master_password()?;
+    let password = input::master_password()?;
+    file::delete(None, &password, &res)?;
 
-    let mut f = match file::open(None) {
-        Ok(v) => v,
-        Err(err) => return Err(err.to_string())
-    };
-    let data = match file::extract_data(&mut f) {
-        Ok(v) => v,
-        Err(err) => return Err(err.to_string())
-    };
-    let decrypted_content = file::decrypt(
-        &master_password,
-        data.buf,
-        data.nonce,
-    )?;
-
-    let mut data = vec![];
-    let mut found: bool = false;
-    let mut res_idx: usize = 0;
-    let mut user_idx: usize = 0;
-    let mut pass_idx: usize = 0;
-    let lines: Vec<&str> = decrypted_content.lines().collect();
-    for (i, _) in lines.iter().enumerate() {
-        if lines[i] == "\n" {
-            continue
-        }
-        if lines[i] == input::RESERVED_RESOURCE && lines[i+1] == resource_str {
-            found = true;
-            res_idx  = i+1;
-            user_idx = i+2;
-            pass_idx = i+3;
-            continue
-        }
-        if found && i == res_idx || i == user_idx || i == pass_idx {
-            continue
-        } else {
-            data.push(String::from(lines[i]));
-        }
-    }
-    if !found {
-        return Err("resource not found".to_string())
-    }
-    let new_nonce = ChaCha20Poly1305::generate_nonce(&mut OsRng);
-    let encrypted_content = file::encrypt(&master_password, &data.join("\n"), new_nonce)?;
-    let mut truncated_file = match file::open_truncate(None) {
-        Ok(v) => v,
-        Err(err) => return Err(err.to_string()) 
-    };
-    if let Err(err) = truncated_file.write_all(&new_nonce.to_vec()) {
-        return Err(err.to_string())
-    };
-    if let Err(err) = truncated_file.write_all(&encrypted_content) {
-        return Err(err.to_string())
-    };
     DONE.store(true, Ordering::Relaxed);
     Ok(())
 }
