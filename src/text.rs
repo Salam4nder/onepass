11b2bbcfd0cae2a9d57792d90a4ef5627a9f722c
Salam4nder <kamran.personal@icloud.com>
2025-04-26 20:09:36 +0200

    chore: string literals

--- src/text.rs
+++ src/text.rs
@@ -1,4 +1,4 @@
-pub const MSG_SETUP:             &str = "Initialised application file. You are all setup! Run `onepass help`";
+pub const MSG_NO_RESOURCES:      &str = "No resources saved - create one with `onepass new`";
 pub const MSG_COMMAND_GET:       &str = "Get resource: e.g - onepass get <resource>";
 pub const MSG_COMMAND_DEL:       &str = "Delete resource: e.g - onepass del <resource>";
 pub const MSG_COMMAND_UPDATE:    &str = "Update resource: e.g - onepass update <resource>";
@@ -10,3 +10,6 @@ pub const MSG_HELP: &str = "COMMANDS:
     update - update a resouruce - its name, username or password
     suggest - suggest a new strong password
 ";
+
+pub const RESERVED_NONCE:    &str = "nonce";
+pub const RESERVED_RESOURCE: &str = "resource";
