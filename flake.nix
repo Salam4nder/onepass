44a0dcb38cecf21cba8b365f9f91aeed6cd539fa
Kamran <kamran.personal@icloud.com>
2025-11-29 20:05:13 +0100

    fix: flake

--- flake.nix
+++ flake.nix
@@ -1,26 +1,37 @@
 {
-        inputs = {
-                nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-                flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.nixfmt
+
+            pkgs.cargo
+            pkgs.clippy
+            pkgs.rustc
+            pkgs.rustfmt
+            pkgs.rust-analyzer
+          ];
+
+          shellHook = ''
+            echo Entered the onepass development environment.
+            export RUST_LOG=debug
+          '';
         };
-
-        outputs = {nixpkgs, flake-utils}:
-        flake-utils.lib.eachDefaultSystem(system:
-          let
-            pkgs = import nixpkgs { inherit system; };
-          in
-          {
-            devShells.default = pkgs.mkShell {
-              packages = [
-                pkgs.rustc
-                pkgs.cargo
-              ];
-
-              shellHook = ''
-                echo "hello world!"
-              '';
-            };
-          }
-        );
+      }
+    );
 }
-
