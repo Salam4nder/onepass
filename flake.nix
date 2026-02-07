d1e3dfd5a8628106aa5552190731622a4b181854
spooksoftware <kamran.personal@proton.me>
2026-02-07 23:39:16 +0100

    feat: rust overlay

--- flake.nix
+++ flake.nix
@@ -2,36 +2,36 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
+    { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlays.default ];
+        };
+
+        rust = pkgs.rust-bin.stable.latest.default;
       in
       {
         devShells.default = pkgs.mkShell {
           packages = [
             pkgs.nixfmt
-
-            pkgs.cargo
-            pkgs.clippy
-            pkgs.rustc
-            pkgs.rustfmt
-            pkgs.rust-analyzer
+            rust
           ];
 
           shellHook = ''
-            echo Entered the onepass development environment.
+            echo Entered the development environment.
             export RUST_LOG=debug
             alias cr='cargo r'
+            alias ct='cargo t'
+            alias ctn='cargo t -- --nocapture'
             alias cc='cargo c'
+            export RUST_BACKTRACE=1
+            export PS1="$PS1[nix-shell] "
           '';
         };
       }
